3d0d25608a2cf1c676c5bc3ec8b63e0422167e49
Alain Scialoja <me@alainscialoja.com>
2018-11-07 12:08:12 +0100

    plot diagram and intermediate steps

--- Bifurcation diagram.py
+++ Bifurcation diagram.py
@@ -5,53 +5,98 @@
 
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 # In[ ]:
 
 
 # Logistic function implementation
-def logis(r,x):
+def logistic_eq(r,x):
     return r*x*(1-x)
 
 
 # In[ ]:
 
 
-# Iterate the function for a given r
-def logis_map(seed, r, n_iter, n_skip=0):
-    values = []
-    x = logis(r,seed);
-    for i in range(n_iter+n_skip-1):
-        x = logis(r,x);
-        if i >= n_skip:
-            values.append(x);
-    return values
+# Show the logistic function
+x = np.linspace(0, 1)
+plt.plot(x, logistic_eq(2, x), 'k')
+plt.show()
+
+# logistic_eq(4.0009, 0.75)
 
 
 # In[ ]:
 
 
-# Test for seed of 0.2, growth rate of 3.05 returning 10 iterations after skipping 1000
-print(logis_map(0.2, 3.05, 10, 1000))
+# Iterate the function for a given r
+def logistic_equation_orbit(seed, r, n_iter, n_skip=0):
+    print('Orbit for seed {0}, growth rate of {1}, plotting {2} iterations after skipping {3}'.format(seed, r, n_iter, n_skip))
+    X_t=[]
+    T=[]
+    t=0
+    x = seed;
+    # Iterate the logistic equation, printing only if n_skip steps have been skipped
+    for i in range(n_iter + n_skip):
+        if i >= n_skip:
+            X_t.append(x)
+            T.append(t)
+            t+=1
+        x = logistic_eq(r,x);
+    # Configure and decorate the plot
+    plt.plot(T, X_t)
+    plt.ylim(0, 1)
+    plt.xlim(0, T[-1])
+    plt.xlabel('Time t')
+    plt.ylabel('X_t')
+    plt.show()
+
+
+# In[ ]:
+
+
+logistic_equation_orbit(0.1, 3.05, 100)
+logistic_equation_orbit(0.1, 3.9, 100)
+logistic_equation_orbit(0.1, 3.9, 100, 1000)
 
 
 # In[ ]:
 
 
 # Create the bifurcation diagram
-def bifurcation_diagram(seed, n_skip, n_iter, step=0.001):
+def bifurcation_diagram(seed, n_skip, n_iter, step=0.0001, r_min=0):
     print("Starting with x0 seed {0}, skip plotting first {1} iterations, then plot next {2} iterations.".format(seed, n_skip, n_iter));
-    r_range = np.linspace(0, 1, num=int(1/step))
-    bif_diagram = []
+    # Array of r values, the x axis of the bifurcation plot
+    R = []
+    # Array of x_t values, the y axis of the bifurcation plot
+    X = []
+    
+    # Create the r values to loop. For each r value we will plot n_iter points
+    r_range = np.linspace(r_min, 4, int(1/step))
+
     for r in r_range:
-        bif_diagram.append(logis_map(seed, r, n_iter, n_skip))
-    return bif_diagram
+        x = seed;
+        # For each r, iterate the logistic function and collect datapoint if n_skip iterations have occurred
+        for i in range(n_iter+n_skip+1):
+            if i >= n_skip:
+                R.append(r)
+                X.append(x)
+                
+            x = logistic_eq(r,x);
+    # Plot the data    
+    plt.plot(R, X, ls='', marker=',')
+    plt.ylim(0, 1)
+    plt.xlim(r_min, 4)
+    plt.xlabel('r')
+    plt.ylabel('X')
+    plt.show()
 
 
 # In[ ]:
 
 
-diagram = bifurcation_diagram(0.2, 10, 100)
-print(diagram)
+bifurcation_diagram(0.2, 100, 5)
+bifurcation_diagram(0.2, 100, 10)
+bifurcation_diagram(0.2, 100, 10, r_min=2.8)
 
--- README.md
+++ README.md
@@ -2,12 +2,15 @@
 
 Following the online course "[Introduction to Dynamical Systems and Chaos](https://www.complexityexplorer.org/courses/91-introduction-to-dynamical-systems-and-chaos)" from Santa Fe Institute, I decided to attempt my own implementation of bifurcation diagrams.
 
+Below the instructions on how to setup and run the project. If you're only interested in the results, just open `Bifurcation diagram.html`.
+
 ### Pre-requisite
 
 Tested on OsX:
 - Python 3.65
 - [Jupyter Notebook](http://jupyter.org/)
 - Numpy
+- Matplotlib
 
 ### Setup
 
@@ -21,3 +24,7 @@ jupyter notebook
 ```
 
 A browser window will open. Select "Bifurcation diagram.ipynb" to run the notebook.
+
+### Preview
+
+![Notbeook preview](/Bifurcation_diagram)
